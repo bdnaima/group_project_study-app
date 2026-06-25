a5f474a828b9fe4104fb5e763aff2b0d9a535d92
Naima Malik <noma_m@hotmail.com>
2026-06-25 11:49:40 +0200

    Add content for login and register pages and css.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router-dom";
+import { FaBook } from "react-icons/fa";
+import "./Auth.css";
+
 const Login = () => {
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className="auth-page">
+      <div className="auth-card">
+        <div className="logo-container">
+          <div className="logo-placeholder">
+            <FaBook />
+          </div>
+        </div>
+
+        <h2 className="title">Study Planner</h2>
+        <p className="subtitle">Plan your tasks. Achieve your goals.</p>
+
+        <form className="auth-form">
+          <input type="email" placeholder="Email" />
+
+          <input type="password" placeholder="Password" />
+
+          <div className="auth-options">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+
+            <a href="/">Forgot password?</a>
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+
+        <p className="switch-auth">
+          Don't have an account? <Link to="/register">Sign up</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,35 @@
+import { FaBook } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Register = () => {
   return (
-    <div>
-      <h2>Register Page</h2>
+    <div className="auth-page">
+      <div className="auth-card">
+        <div className="logo-container">
+          <div className="logo-placeholder">
+            <FaBook />
+          </div>
+        </div>
+
+        <h2>Create Account</h2>
+        <p className="subtitle">Start organizing your studies.</p>
+
+        <form className="auth-form">
+          <input type="text" placeholder="Full Name" />
+
+          <input type="email" placeholder="Email" />
+
+          <input type="password" placeholder="Password" />
+
+          <input type="password" placeholder="Confirm Password" />
+
+          <button type="submit">Sign Up</button>
+        </form>
+
+        <p className="switch-auth">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
