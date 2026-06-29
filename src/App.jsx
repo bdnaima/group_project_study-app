2b9334ed62b1bfc66bb30f300d37371715f415eb
Christian Stråhle <thebonemeal23@gmail.com>
2026-06-29 10:18:44 +0200

    Till navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // 1. Vi importerar din nya Navbar här
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -8,6 +9,9 @@ import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
+      {/* 2. Navbaren placeras här, så visas den på alla sidor automatiskt */}
+      <Navbar /> 
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
