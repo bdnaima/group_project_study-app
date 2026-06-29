a1b77ffcf48cbbd0d59e7edb6a05a0a5e2bbc33a
Christian Stråhle <thebonemeal23@gmail.com>
2026-06-29 10:27:50 +0200

    Fixa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
+      <BrowserRouter>
+          <Navbar />
+          <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
